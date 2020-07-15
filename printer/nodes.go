b48f85236cb60e337101c541234dcc1c253e1e96
Zaba505 <cakub6@gmx.com>
2020-07-14 22:51:50 -0400

    feat(printer): rough out api

---
+++ printer/nodes.go
@@ -0,0 +1,7 @@
+package printer
+
+import (
+	"github.com/gqlc/graphql/ast"
+)
+
+func (p *printer) args(args []*ast.Arg) {}
---
+++ printer/printer_test.go
@@ -0,0 +1,73 @@
+package printer
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/gqlc/graphql/parser"
+	"github.com/gqlc/graphql/token"
+)
+
+func TestFprint_Document(t *testing.T) {
+	docStr := `# Top Comment
+
+@import(paths: ["hello.gql"])
+
+scalar String`
+
+	dset := token.NewDocSet()
+	doc, err := parser.ParseDoc(dset, "test", strings.NewReader(docStr), 0)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var b strings.Builder
+	err = Fprint(&b, dset, doc.Types)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if b.String() != docStr {
+		t.Logf("\nexpected: %s\ngot: %s", docStr, b.String())
+		t.Fail()
+		return
+	}
+}
+
+func TestFprint_TypeDecls(t *testing.T) {
+	testCases := []struct {
+		Name string
+		Src  string
+	}{
+		{
+			Name: "Scalar",
+			Src:  "scalar String",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(subT *testing.T) {
+			dset := token.NewDocSet()
+			doc, err := parser.ParseDoc(dset, testCase.Name, strings.NewReader(testCase.Src), 0)
+			if err != nil {
+				subT.Error(err)
+				return
+			}
+
+			var b strings.Builder
+			err = Fprint(&b, dset, doc.Types)
+			if err != nil {
+				subT.Error(err)
+				return
+			}
+
+			if b.String() != testCase.Src {
+				subT.Logf("\nexpected: %s\ngot: %s", testCase.Src, b.String())
+				subT.Fail()
+				return
+			}
+		})
+	}
+}
