e07069a3b6144370f132447d3ed7dfc6dfd54490
Zaba505 <cakub6@gmx.com>
2020-07-27 15:29:48 -0400

    feat(intro-scanner): tokenize interface defintions

--- parser/intro.go
+++ parser/intro.go
@@ -14,11 +14,52 @@ func scanIntrospect(doc *token.Doc, name string, src io.Reader) lexer.Interface 
 		dec:   json.NewDecoder(src),
 		doc:   doc,
 		items: make(chan lexer.Item, 2),
+		buf:   make(itemBuf, 0, 12),
 	}
 
 	go s.run()
 
 	return s
+}
+
+// itemBuf is a priority queue for ordering lexer.Items after
+// producing them from JSON obj keys that are not gauranteed
+// to be in order.
+//
+type itemBuf []struct {
+	priority int
+	item     lexer.Item
+}
+
+func (b *itemBuf) insert(priority int, item lexer.Item) {
+	*b = append(*b, struct {
+		priority int
+		item     lexer.Item
+	}{priority, item})
+
+	// sort on insert since JSON keys should be sorted based on query
+	// thus meaning this should only be one comparision on best average.
+	b.sort()
+}
+
+func (b *itemBuf) sort() {
+	if len(*b) == 1 {
+		return
+	}
+
+	for i := len(*b) - 1; i > 0; i-- {
+		j := i
+		for j > 0 {
+			j--
+			if (*b)[i].priority >= (*b)[j].priority {
+				break
+			}
+
+			k := (*b)[j]
+			(*b)[j] = (*b)[i]
+			(*b)[i] = k
+		}
+	}
 }
 
 type introScanner struct {
@@ -28,8 +69,6 @@ type introScanner struct {
 	doc  *token.Doc
 
 	pos   int
-	start int
-	width int
 	line  int
 	items chan lexer.Item
 
@@ -42,7 +81,7 @@ type introScanner struct {
 	//		}
 	// 	]
 	//
-	itemBuf []lexer.Item
+	buf itemBuf
 }
 
 func (s *introScanner) NextItem() lexer.Item {
@@ -69,17 +108,16 @@ func (s *introScanner) run() {
 
 func (s *introScanner) emit(t token.Token, val string) {
 	s.items <- lexer.Item{
-		Pos:  s.doc.Pos(s.start),
+		Pos:  s.doc.Pos(s.pos),
 		Line: s.line,
 		Typ:  t,
 		Val:  val,
 	}
-	s.start = s.pos
+	s.pos += len(val)
 }
 
 func (s *introScanner) emitItem(item lexer.Item) {
 	s.items <- item
-	s.start = s.pos
 }
 
 // json.Token never represents a struct
@@ -148,41 +186,76 @@ func scanDirectives(s *introScanner) stateFn {
 func scanTypes(s *introScanner) stateFn {
 	s.expect(json.Delim('['), "types opening")
 
-	tok := s.next()
-	for tok != json.Delim(']') {
+	for {
+		s.line += 1
+
+		tok := s.next()
+		if tok == json.Delim(']') {
+			return scanDoc
+		}
 		if tok != json.Delim('{') {
 			s.unexpected(tok, "type opening")
 		}
 
-		tok = s.tokenizeTypeDecl()
-		for _, item := range s.itemBuf {
-			item.Pos = token.Pos(s.pos) + 1
-			s.pos += len(item.Val) + 1
-			s.emitItem(item)
-		}
-		if tok != json.Delim('}') {
-			s.unexpected(tok, "type closing")
-		}
+		s.tokenizeTypeDecl()
 
-		tok = s.next()
+		s.emitBuf()
 	}
-	return scanDoc
 }
 
-func (s *introScanner) tokenizeTypeDecl() json.Token {
-	buf := make([]lexer.Item, 0, 12) // "descr" ident("descr" arg: Type = "default"): Type
+func (s *introScanner) emitBuf() {
+	inArgs := false
+	for i, itemP := range s.buf {
+		item := itemP.item
 
-	description, kind, name := -1, -1, -1
-	interfaces, fields := -1, -1
+		item.Pos = token.Pos(s.pos) + 1
+		s.pos += len(item.Val)
 
-	tok := s.next()
-	for tok != json.Delim('}') {
+		if i == len(s.buf)-1 {
+			s.emitItem(item)
+			break
+		}
+
+		switch {
+		case item.Typ == token.Token_LPAREN, item.Typ == token.Token_RPAREN:
+			inArgs = !inArgs
+			s.pos -= 1
+			item.Pos -= 1
+		case !inArgs && (item.Typ == token.Token_COMMA || item.Typ == token.Token_LBRACE):
+			s.pos += 2
+		case inArgs && item.Typ == token.Token_COMMA:
+			s.pos += 1
+		default:
+			if s.buf[i+1].item.Typ != token.Token_COLON {
+				s.pos += 1
+			}
+		}
+
+		s.emitItem(item)
+	}
+	s.buf = s.buf[:0]
+}
+
+func (s *introScanner) tokenizeTypeDecl() {
+	buf := make(itemBuf, 0, 12) // "descr" ident("descr" arg: Type = "default"): Type
+
+	// Priorities:
+	// 0 - description
+	// 1 - kind
+	// 2 - name
+	// 3 - interfaces, =
+	// 4 - fields, members, enum values
+
+	for {
+		tok := s.next()
+		if tok == json.Delim('}') {
+			return
+		}
+
 		switch tok {
 		case "kind":
-			kind = description + 1
-			s.tokenizeKind(&buf)
-			s.itemBuf = append(s.itemBuf[:kind], append(buf, s.itemBuf[kind:]...)...)
-			buf = buf[:0]
+			item := s.tokenizeKind()
+			s.buf.insert(1, item)
 		case "name":
 			tok = s.next()
 			if tok == nil {
@@ -194,11 +267,7 @@ func (s *introScanner) tokenizeTypeDecl() json.Token {
 				s.unexpected(n, "name")
 			}
 
-			name = description + kind + 2
-
-			s.itemBuf = append(s.itemBuf, lexer.Item{})
-			copy(s.itemBuf[name+1:], s.itemBuf[name:])
-			s.itemBuf[name] = lexer.Item{Typ: token.Token_IDENT, Val: n}
+			s.buf.insert(2, lexer.Item{Typ: token.Token_IDENT, Val: n})
 		case "description":
 			tok = s.next()
 			if tok == nil {
@@ -209,23 +278,26 @@ func (s *introScanner) tokenizeTypeDecl() json.Token {
 				s.unexpected(descr, "description")
 			}
 
-			description = 0
-			s.itemBuf = append(s.itemBuf, lexer.Item{})
-			copy(s.itemBuf[description+1:], s.itemBuf[description:])
-			s.itemBuf[description] = lexer.Item{Typ: token.Token_DESCRIPTION, Val: descr}
+			s.buf.insert(0, lexer.Item{Typ: token.Token_DESCRIPTION, Val: descr})
 		case "fields":
 			tok = s.next()
 			if tok == nil {
 				break
 			}
 			if tok != json.Delim('[') {
-				s.unexpected(tok, "fields")
+				s.unexpected(tok, "fields opening")
 			}
 
-			fields = description + kind + name + interfaces + 4
-			s.tokenizeFields(&buf)
-			s.itemBuf = append(s.itemBuf[:fields], append(buf, s.itemBuf[fields:]...)...)
-			buf = buf[:0]
+			s.buf.insert(4, lexer.Item{Typ: token.Token_LBRACE, Val: "{"})
+			s.tokenizeObjList(&buf, "fields closing", s.tokenizeField)
+
+			buf = buf[:len(buf)-1]
+			for _, i := range buf {
+				s.buf.insert(i.priority+4, i.item)
+			}
+			s.buf.insert(4+len(buf), lexer.Item{Typ: token.Token_RBRACE, Val: "}"})
+
+			s.line += 2
 		case "interfaces":
 			tok = s.next()
 			if tok == nil {
@@ -248,41 +320,287 @@ func (s *introScanner) tokenizeTypeDecl() json.Token {
 			}
 		case "ofType":
 			tok = s.next()
-			if tok == nil {
-				break
+			if tok != nil {
+				s.unexpected(tok, "ofType should be null for a type declaration")
 			}
+		default:
+			s.unexpected(tok, "type")
 		}
-		tok = s.next()
 	}
-	return tok
 }
 
-func (s *introScanner) tokenizeKind(items *[]lexer.Item) {
+func (s *introScanner) tokenizeKind() lexer.Item {
 	kind := s.next()
 	switch kind {
 	case "SCALAR":
-		*items = append(*items, lexer.Item{Typ: token.Token_SCALAR, Val: "scalar"})
+		return lexer.Item{Typ: token.Token_SCALAR, Val: "scalar", Line: s.line}
 	case "OBJECT":
-		*items = append(*items, lexer.Item{Typ: token.Token_TYPE, Val: "type"})
+		return lexer.Item{Typ: token.Token_TYPE, Val: "type", Line: s.line}
 	case "INTERFACE":
-		*items = append(*items, lexer.Item{Typ: token.Token_INTERFACE, Val: "interface"})
+		return lexer.Item{Typ: token.Token_INTERFACE, Val: "interface", Line: s.line}
 	case "UNION":
-		*items = append(*items, lexer.Item{Typ: token.Token_UNION, Val: "union"})
+		return lexer.Item{Typ: token.Token_UNION, Val: "union", Line: s.line}
 	case "ENUM":
-		*items = append(*items, lexer.Item{Typ: token.Token_ENUM, Val: "enum"})
+		return lexer.Item{Typ: token.Token_ENUM, Val: "enum", Line: s.line}
 	case "INPUT_OBJECT":
-		*items = append(*items, lexer.Item{Typ: token.Token_INPUT, Val: "input"})
+		return lexer.Item{Typ: token.Token_INPUT, Val: "input", Line: s.line}
 	default:
-		s.errorf("unknown type kind: %s", kind)
+		s.unexpected(kind, "unknown type kind")
+		return lexer.Item{}
 	}
 }
 
-func (s *introScanner) tokenizeFields(items *[]lexer.Item) {
+func (s *introScanner) tokenizeObjList(items *itemBuf, context string, f func(i int, items *itemBuf)) {
+	i := 0
+	for {
+		tok := s.next()
+		if tok == json.Delim(']') {
+			return
+		}
+		if tok != json.Delim('{') {
+			s.unexpected(tok, context)
+		}
+
+		f(i, items)
+		i++
+	}
+}
+
+func (s *introScanner) tokenizeField(i int, items *itemBuf) {
+	s.line += 1
+	buf := make(itemBuf, 0, 12)
+
+	// Priorities
+	// 0 - description
+	// 1 - name
+	// 2 - args
+	// 3 - type sig
+	// 4 - deprecated directive
+
+	for {
+		tok := s.next()
+		if tok == json.Delim('}') {
+			items.insert(3, lexer.Item{Typ: token.Token_COMMA, Val: ",", Line: i + 1})
+			return
+		}
+
+		switch tok {
+		case "description":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			descr, ok := tok.(string)
+			if !ok {
+				s.unexpected(descr, "description")
+			}
+
+			items.insert(0, lexer.Item{Typ: token.Token_DESCRIPTION, Val: descr, Line: 2 * i})
+		case "name":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			n, ok := tok.(string)
+			if !ok {
+				s.unexpected(n, "name")
+			}
+
+			items.insert(1, lexer.Item{Typ: token.Token_IDENT, Val: n, Line: 2*i + 1})
+		case "args":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			if tok != json.Delim('[') {
+				s.unexpected(tok, "args opening")
+			}
+
+			s.tokenizeObjList(&buf, "args closing", s.tokenizeInputValue)
+
+			items.insert(2, lexer.Item{Typ: token.Token_LPAREN, Val: "(", Line: 2*i + 1})
+			buf = buf[:len(buf)-1]
+			for _, it := range buf {
+				it.item.Line = 2*i + 1
+				items.insert(2, it.item)
+			}
+			items.insert(2, lexer.Item{Typ: token.Token_RPAREN, Val: ")", Line: 2*i + 1})
+			items.insert(2, lexer.Item{Typ: token.Token_COLON, Val: ":", Line: 2*i + 1})
+			buf = buf[:0]
+		case "type":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			if tok != json.Delim('{') {
+				s.unexpected(tok, "field type signature")
+			}
+
+			s.tokenizeTypeSig(&buf)
+
+			for _, it := range buf {
+				it.item.Line = i + 1
+				items.insert(3, it.item)
+			}
+			buf = buf[:0]
+		case "isDeprecated":
+			s.next()
+			// TODO
+		case "deprecationReason":
+			s.next()
+			// TODO
+		default:
+			s.unexpected(tok, "field")
+		}
+	}
+}
+
+func (s *introScanner) tokenizeInputValue(i int, items *itemBuf) {
+	buf := make(itemBuf, 0, 5)
+
+	// Priorites
+	// 0 - description
+	// 1 - name
+	// 2 - type signature
+	// 3 - default value
+
+	for {
+		tok := s.next()
+		if tok == json.Delim('}') {
+			items.insert(3, lexer.Item{Typ: token.Token_COMMA, Val: ",", Line: i + 1})
+			return
+		}
+
+		switch tok {
+		case "name":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			n, ok := tok.(string)
+			if !ok {
+				s.unexpected(n, "name")
+			}
+
+			items.insert(1, lexer.Item{Typ: token.Token_IDENT, Val: n})
+			items.insert(1, lexer.Item{Typ: token.Token_COLON, Val: ":"})
+		case "description":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			descr, ok := tok.(string)
+			if !ok {
+				s.unexpected(descr, "description")
+			}
+
+			items.insert(0, lexer.Item{Typ: token.Token_DESCRIPTION, Val: descr})
+		case "type":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			if tok != json.Delim('{') {
+				s.unexpected(tok, "description")
+			}
+
+			s.tokenizeTypeSig(&buf)
+
+			for _, i := range buf {
+				items.insert(2, i.item)
+			}
+			buf = buf[:0]
+		case "defaultValue":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			defaultVal, ok := tok.(string)
+			if !ok {
+				s.unexpected(defaultVal, "description")
+			}
+
+			items.insert(3, lexer.Item{Typ: token.Token_ASSIGN, Val: "="})
+			// TODO: Tokenize defaultValue
+		default:
+			s.unexpected(tok, "input value")
+		}
+	}
+}
+
+type signature uint
+
+const (
+	unknown signature = iota
+	ident
+	list
+	nonNull
+)
+
+func (s *introScanner) tokenizeTypeSig(items *itemBuf) {
+	// signa := unknown
+
 	for {
 		tok := s.next()
 		if tok == json.Delim('}') {
 			return
 		}
 
+		switch tok {
+		case "kind":
+			tok = s.next()
+			if tok == nil {
+				s.unexpected(tok, "type signature kind must be non-null")
+			}
+
+			// sig, ok := tok.(string)
+			// if !ok {
+			// 	s.unexpected(tok, "type signature kind should be a string")
+			// }
+			// signa = ident
+
+			// if sig == "LIST" {
+			// 	signa = list
+			// 	*items = append(*items, lexer.Item{})
+			// 	copy((*items)[1:], (*items)[0:])
+			// 	(*items)[0] = lexer.Item{Typ: token.Token_LBRACK, Val: "["}
+			// }
+			//
+			// if sig == "NON_NULL" {
+			// 	signa = nonNull
+			// }
+		case "name":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+
+			name, ok := tok.(string)
+			if !ok {
+				s.unexpected(tok, "type signature type name should be a string")
+			}
+			if name == "" {
+				break
+			}
+
+			items.insert(0, lexer.Item{Typ: token.Token_IDENT, Val: name})
+		case "ofType":
+			tok = s.next()
+			if tok == nil {
+				break
+			}
+			// if tok != json.Delim('{') {
+			// 	s.unexpected(tok, "type signature ofType")
+			// }
+			//
+			// s.tokenizeTypeSig(items)
+			//
+			// if signa == list {
+			// 	*items = append(*items, lexer.Item{Typ: token.Token_RBRACK, Val: "]"})
+			// }
+			// if signa == nonNull {
+			// 	*items = append(*items, lexer.Item{Typ: token.Token_NOT, Val: "!"})
+			// }
+		}
 	}
 }
