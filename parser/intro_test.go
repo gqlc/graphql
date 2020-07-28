25ddf3979974970cf22fa7f165bfdddeae5dd71f
Zaba505 <cakub6@gmx.com>
2020-07-28 00:51:11 -0400

    test(intro_scanner): add case for multiple fields and complex args

--- parser/intro_test.go
+++ parser/intro_test.go
@@ -156,6 +156,120 @@ func TestParseIntrospection(t *testing.T) {
 			}
       `,
 		},
+		{
+			Name: "Object With Complex fields",
+			Src: `type Test implements A & B & C {
+	a(b: B!): [A!]!
+	b(c: [C], d: D): E
+}`,
+			Intro: `
+      {
+			  "__schema": {
+			    "directives": [],
+			    "types": [
+			      {
+			        "kind": "OBJECT",
+			        "name": "Test",
+			        "description": null,
+			        "fields": [
+								{
+									"name": "a",
+									"description": null,
+									"args": [
+										{
+											"name": "b",
+											"description": null,
+											"type": {
+												"kind": "NON_NULL",
+												"name": null,
+												"ofType": {
+													"kind": "OBJECT",
+													"name": "B",
+													"ofType": null
+												}
+											},
+											"defaultValue": null
+										}
+									],
+									"type": {
+										"kind": "NON_NULL",
+										"name": null,
+										"ofType": {
+											"kind": "LIST",
+											"name": null,
+											"ofType": {
+												"kind": "NON_NULL",
+												"name": null,
+												"ofType": {
+													"kind": "OBJECT",
+													"name": "A",
+													"ofType": null
+												}
+											}
+										}
+									},
+									"isDeprecated": false,
+									"deprecationReason": null
+								},
+								{
+									"name": "b",
+									"description": null,
+									"args": [
+										{
+											"name": "c",
+											"description": null,
+											"type": {
+												"kind": "LIST",
+												"name": null,
+												"ofType": {
+													"kind": "OBJECT",
+													"name": "C",
+													"ofType": null
+												}
+											},
+											"defaultValue": null
+										},
+										{
+											"name": "d",
+											"description": null,
+											"type": {
+												"kind": "OBJECT",
+												"name": "D",
+												"ofType": null
+											},
+											"defaultValue": null
+										}
+									],
+									"type": {
+										"kind": "OBJECT",
+										"name": "E",
+										"ofType": null
+									},
+									"isDeprecated": false,
+									"deprecationReason": null
+								}
+							],
+			        "interfaces": [
+								{
+									"name": "A"
+								},
+								{
+									"name": "B"
+								},
+								{
+									"name": "C"
+								}
+							],
+			        "possibleTypes": null,
+			        "enumValues": null,
+			        "inputFields": null,
+			        "ofType": null
+			      }
+			    ]
+			  }
+			}
+      `,
+		},
 	}
 
 	for _, testCase := range testCases {
