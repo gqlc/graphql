f96de20d77b7fc689641008473b53f759c1a6096
Zaba505 <cakub6@gmx.com>
2020-07-29 00:29:44 -0400

    test(intro_scanner): make sure default value has correct type

--- parser/intro_test.go
+++ parser/intro_test.go
@@ -428,7 +428,7 @@ func TestParseIntrospection(t *testing.T) {
 		},
 		{
 			Name: "Directive",
-			Src:  "directive @test(a: Int! = 1, b: [Int] = [1,2,3], c: C = {hello: world}) on FIELD_DEFINITION | ENUM_VALUE | INPUT_FIELD_DEFINITION",
+			Src:  "directive @test(a: Int! = 1, b: [Int] = [1,2,3], c: C = {hello: \"world\"}) on FIELD_DEFINITION | ENUM_VALUE | INPUT_FIELD_DEFINITION",
 			Intro: `
 			{
 				"__schema": {
