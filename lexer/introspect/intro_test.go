560435f5d58620fee8128b2c34bb27c0dc36296b
Zaba505 <cakub6@gmx.com>
2020-07-30 23:48:39 -0400

    test(intro_scanner): lex interface declarations

--- lexer/introspect/intro_test.go
+++ lexer/introspect/intro_test.go
@@ -1,23 +1,23 @@
 package introspect
 
 import (
-  "testing"
 	"strings"
+	"testing"
 
+	"github.com/gqlc/graphql/lexer"
 	"github.com/gqlc/graphql/token"
-  "github.com/gqlc/graphql/lexer"
 )
 
 func TestLex(t *testing.T) {
-  testCases := []struct{
-    Name string
-    Src string
-    Intro string
-  }{
-    {
-      Name: "Scalar",
-      Src: "scalar Test",
-      Intro: `
+	testCases := []struct {
+		Name  string
+		Src   string
+		Intro string
+	}{
+		{
+			Name: "Scalar",
+			Src:  "scalar Test",
+			Intro: `
       {
 			  "__schema": {
 			    "directives": [],
@@ -37,40 +37,88 @@ func TestLex(t *testing.T) {
 			  }
 			}
       `,
-    },
-  }
+		},
+		{
+			Name: "Interface",
+			Src: `interface Test {
+	a(b: B): A
+}`,
+			Intro: `
+      {
+			  "__schema": {
+			    "directives": [],
+			    "types": [
+			      {
+			        "kind": "INTERFACE",
+			        "name": "Test",
+			        "description": null,
+			        "fields": [
+								{
+									"name": "a",
+									"description": null,
+									"args": [
+										{
+											"name": "b",
+											"description": null,
+											"type": {
+												"kind": "OBJECT",
+												"name": "B"
+											},
+											"defaultValue": null
+										}
+									],
+									"type": {
+										"kind": "OBJECT",
+										"name": "A"
+									},
+									"isDeprecated": false,
+									"deprecationReason": null
+								}
+							],
+			        "interfaces": null,
+			        "possibleTypes": null,
+			        "enumValues": null,
+			        "inputFields": null,
+			        "ofType": null
+			      }
+			    ]
+			  }
+			}
+      `,
+		},
+	}
 
-  for _, testCase := range testCases {
-    t.Run(testCase.Name, func(subT *testing.T) {
-      exSet := token.NewDocSet()
-      ex := lexer.Lex(exSet.AddDoc("ex", -1, len(testCase.Src)), testCase.Src)
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(subT *testing.T) {
+			exSet := token.NewDocSet()
+			ex := lexer.Lex(exSet.AddDoc("ex", -1, len(testCase.Src)), testCase.Src)
 
-      outSet := token.NewDocSet()
-      out := Lex(outSet.AddDoc("out", -1, 500), "out", strings.NewReader(testCase.Intro))
+			outSet := token.NewDocSet()
+			out := Lex(outSet.AddDoc("out", -1, 500), "out", strings.NewReader(testCase.Intro))
 
-      compare(subT, ex, out)
-    })
-  }
+			compare(subT, ex, out)
+		})
+	}
 }
 
 func compare(t *testing.T, ex, out lexer.Interface) {
-  defer ex.Drain()
-  defer out.Drain()
+	defer ex.Drain()
+	defer out.Drain()
 
-  t.Helper()
+	t.Helper()
 
-  for {
-    e := ex.NextItem()
-    o := out.NextItem()
+	for {
+		e := ex.NextItem()
+		o := out.NextItem()
 
-    if e != o {
-      t.Logf("expected: %#v, got: %#v", e, o)
-      t.Fail()
-      return
-    }
+		if e != o {
+			t.Logf("expected: %#v, got: %#v", e, o)
+			t.Fail()
+			return
+		}
 
-    if e.Typ == token.Token_EOF {
-      return
-    }
-  }
+		if e.Typ == token.Token_EOF {
+			return
+		}
+	}
 }
