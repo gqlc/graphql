6e6c725a2d182cf4353260bdec5deecf1d4854d6
Zaba505 <cakub6@gmx.com>
2020-07-31 16:08:31 -0400

    test(lexer/introspect): mock a full response with multiple types/directives

--- lexer/introspect/intro_test.go
+++ lexer/introspect/intro_test.go
@@ -498,11 +498,369 @@ func TestLex(t *testing.T) {
 			ex := lexer.Lex(exSet.AddDoc("ex", -1, len(testCase.Src)), testCase.Src)
 
 			outSet := token.NewDocSet()
-			out := Lex(outSet.AddDoc("out", -1, 500), "out", strings.NewReader(testCase.Intro))
+			out := Lex(outSet.AddDoc("out", -1, 500), strings.NewReader(testCase.Intro))
 
 			compare(subT, ex, out)
 		})
 	}
+}
+
+var intro = `{
+	"__schema": {
+		"directives": [
+			{
+				"description": null,
+				"name": "sorted",
+				"locations": [
+					"FIELD_DEFINITION"
+				],
+				"args": [],
+				"isRepeatable": false
+			},
+			{
+				"description": null,
+				"name": "test",
+				"locations": [
+					"FIELD_DEFINITION",
+					"ENUM_VALUE",
+					"INPUT_FIELD_DEFINITION"
+				],
+				"args": [
+					{
+						"name": "a",
+						"description": null,
+						"type": {
+							"kind": "NON_NULL",
+							"name": null,
+							"ofType": {
+								"kind": "SCALAR",
+								"name": "Int",
+								"ofType": null
+							}
+						},
+						"defaultValue": "1"
+					},
+					{
+						"name": "b",
+						"description": null,
+						"type": {
+							"kind": "LIST",
+							"name": null,
+							"ofType": {
+								"kind": "SCALAR",
+								"name": "Int",
+								"ofType": null
+							}
+						},
+						"defaultValue": "[1,2,3]"
+					},
+					{
+						"name": "c",
+						"description": null,
+						"type": {
+							"kind": "OBJECT",
+							"name": "C",
+							"ofType": null
+						},
+						"defaultValue": "{hello:\"world\",good:\"bye\"}"
+					}
+				],
+				"isRepeatable": false
+			}
+		],
+		"types": [
+			{
+				"kind": "SCALAR",
+				"name": "Test",
+				"description": null,
+				"fields": null,
+				"interfaces": null,
+				"possibleTypes": null,
+				"enumValues": null,
+				"inputFields": null,
+				"ofType": null
+			},
+			{
+				"kind": "INTERFACE",
+				"name": "Test",
+				"description": null,
+				"fields": [
+					{
+						"name": "a",
+						"description": null,
+						"args": [
+							{
+								"name": "b",
+								"description": null,
+								"type": {
+									"kind": "OBJECT",
+									"name": "B"
+								},
+								"defaultValue": null
+							}
+						],
+						"type": {
+							"kind": "OBJECT",
+							"name": "A"
+						},
+						"isDeprecated": false,
+						"deprecationReason": null
+					}
+				],
+				"interfaces": null,
+				"possibleTypes": null,
+				"enumValues": null,
+				"inputFields": null,
+				"ofType": null
+			},
+			{
+				"kind": "OBJECT",
+				"name": "Test",
+				"description": null,
+				"fields": [
+					{
+						"name": "a",
+						"description": null,
+						"args": [
+							{
+								"name": "b",
+								"description": null,
+								"type": {
+									"kind": "NON_NULL",
+									"name": null,
+									"ofType": {
+										"kind": "OBJECT",
+										"name": "B",
+										"ofType": null
+									}
+								},
+								"defaultValue": null
+							}
+						],
+						"type": {
+							"kind": "NON_NULL",
+							"name": null,
+							"ofType": {
+								"kind": "LIST",
+								"name": null,
+								"ofType": {
+									"kind": "NON_NULL",
+									"name": null,
+									"ofType": {
+										"kind": "OBJECT",
+										"name": "A",
+										"ofType": null
+									}
+								}
+							}
+						},
+						"isDeprecated": false,
+						"deprecationReason": null
+					},
+					{
+						"name": "b",
+						"description": null,
+						"args": [
+							{
+								"name": "c",
+								"description": null,
+								"type": {
+									"kind": "LIST",
+									"name": null,
+									"ofType": {
+										"kind": "OBJECT",
+										"name": "C",
+										"ofType": null
+									}
+								},
+								"defaultValue": null
+							},
+							{
+								"name": "d",
+								"description": null,
+								"type": {
+									"kind": "OBJECT",
+									"name": "D",
+									"ofType": null
+								},
+								"defaultValue": null
+							}
+						],
+						"type": {
+							"kind": "OBJECT",
+							"name": "E",
+							"ofType": null
+						},
+						"isDeprecated": false,
+						"deprecationReason": null
+					}
+				],
+				"interfaces": [
+					{
+						"name": "A"
+					},
+					{
+						"name": "B"
+					},
+					{
+						"name": "C"
+					}
+				],
+				"possibleTypes": null,
+				"enumValues": null,
+				"inputFields": null,
+				"ofType": null
+			},
+			{
+				"kind": "UNION",
+				"name": "Test",
+				"description": null,
+				"fields": null,
+				"interfaces": null,
+				"possibleTypes": [
+					{
+						"name": "A"
+					},
+					{
+						"name": "B"
+					},
+					{
+						"name": "C"
+					}
+				],
+				"enumValues": null,
+				"inputFields": null,
+				"ofType": null
+			},
+			{
+				"kind": "ENUM",
+				"name": "Test",
+				"description": null,
+				"fields": null,
+				"interfaces": null,
+				"possibleTypes": null,
+				"enumValues": [
+					{
+						"description": null,
+						"name": "A",
+						"isDeprecated": null,
+						"deprecationReason": null
+					},
+					{
+						"description": null,
+						"name": "B",
+						"isDeprecated": null,
+						"deprecationReason": null
+					},
+					{
+						"description": null,
+						"name": "C",
+						"isDeprecated": null,
+						"deprecationReason": null
+					}
+				],
+				"inputFields": null,
+				"ofType": null
+			},
+			{
+				"kind": "INPUT_OBJECT",
+				"name": "Test",
+				"description": null,
+				"fields": null,
+				"interfaces": null,
+				"possibleTypes": null,
+				"enumValues": null,
+				"inputFields": [
+					{
+						"name": "a",
+						"description": null,
+						"type": {
+							"kind": "NON_NULL",
+							"name": null,
+							"ofType": {
+								"kind": "LIST",
+								"name": null,
+								"ofType": {
+									"kind": "NON_NULL",
+									"name": null,
+									"ofType": {
+										"kind": "OBJECT",
+										"name": "A",
+										"ofType": null
+									}
+								}
+							}
+						},
+						"defaultValue": null
+					},
+					{
+						"name": "b",
+						"description": null,
+						"type": {
+							"kind": "SCALAR",
+							"name": "String",
+							"ofType": null
+						},
+						"defaultValue": "\"hello\""
+					},
+					{
+						"name": "c",
+						"description": null,
+						"type": {
+							"kind": "NON_NULL",
+							"name": null,
+							"ofType": {
+								"kind": "SCALAR",
+								"name": "Int",
+								"ofType": null
+							}
+						},
+						"defaultValue": "1"
+					}
+				],
+				"ofType": null
+			}
+		]
+	}
+}`
+
+func TestLex_All(t *testing.T) {
+	src := `directive @sorted on FIELD_DEFINITION
+
+directive @test(a: Int! = 1, b: [Int] = [1,2,3], c: C = {hello: "world",good: "bye"}) on FIELD_DEFINITION | ENUM_VALUE | INPUT_FIELD_DEFINITION
+
+scalar Test
+
+interface Test {
+	a(b: B): A
+}
+
+type Test implements A & B & C {
+	a(b: B!): [A!]!
+	b(c: [C], d: D): E
+}
+
+union Test = A | B | C
+
+enum Test {
+	A
+	B
+	C
+}
+
+input Test {
+	a: [A!]!
+	b: String = "hello"
+	c: Int! = 1
+}`
+
+	exSet := token.NewDocSet()
+	ex := lexer.Lex(exSet.AddDoc("ex", -1, len(src)), src)
+
+	outSet := token.NewDocSet()
+	out := Lex(outSet.AddDoc("out", -1, 500), strings.NewReader(intro))
+
+	compare(t, ex, out)
 }
 
 func compare(t *testing.T, ex, out lexer.Interface) {
